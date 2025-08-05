e0bd155f6d61fe7354ef2e175aa10f7885dd7cad
localzero97 <150771231+localzero97@users.noreply.github.com>
2025-08-05 13:18:24 +0800

    Update dashboard_guru.js

--- dashboard_guru.js
+++ dashboard_guru.js
@@ -1,7 +1,7 @@
 // =======================================================================
 // PENTING: Ganti dengan URL WEB APP BARU ANDA
 // =======================================================================
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzMQ33BZhTFpJJVJSUA-i6-fh8vgR1Lw-PtrJE-WW0w3-auh8IFnT2MOc2KfHx8lzpa/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwILA8md3Y7YiG5KK0-GMOnshEjXSuXBcFhPDeyFgUvdFq7hZAgmZDPxIlZX-VuJ7Qi/exec";
 
 document.addEventListener("DOMContentLoaded", function() {
     const namaGuruEl = document.getElementById("nama-guru");
